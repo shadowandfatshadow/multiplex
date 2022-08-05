7fa338e6fcfaa3e12acd341c31745a465802e9a0
chan <1>
2022-08-05 15:00:45 +0800

    阴影窗口

---
+++ 阴影窗口/shadowwidget.cpp
@@ -0,0 +1,34 @@
+﻿#include "shadowwidget.h"
+#include <QPainter>
+#include <QPainterPath>
+#include <QtMath>
+
+ShadowWidget::ShadowWidget(QWidget *parent)
+    : QWidget{parent}
+{
+
+}
+
+void ShadowWidget::paintEvent(QPaintEvent *event)
+{
+    if (isMaximized())
+    {
+        return;
+    }
+    int nShadowsWidth = 6;
+    int nRadius = 3;
+    QPainterPath path;
+    path.setFillRule(Qt::WindingFill);
+    path.addRoundedRect(nShadowsWidth, nShadowsWidth, this->width() - nShadowsWidth * 2, this->height() - nShadowsWidth * 2, nRadius, nRadius);
+    QPainter painter(this);
+    QColor color(0, 0, 0, 200);
+    for (int i = 0; i < nShadowsWidth; i++)
+    {
+        QPainterPath path;
+        path.setFillRule(Qt::WindingFill);
+        path.addRoundedRect(nShadowsWidth - i, nShadowsWidth - i, this->width() - (nShadowsWidth - i) * 2, this->height() - (nShadowsWidth - i) * 2, nRadius + i, nRadius + i);
+        color.setAlpha(100 - qSqrt(i) * 50);
+        painter.setPen(color);
+        painter.drawPath(path);
+    }
+}
---
+++ 阴影窗口/shadowwidget.h
@@ -0,0 +1,20 @@
+﻿#ifndef SHADOWWIDGET_H
+#define SHADOWWIDGET_H
+
+#include <QWidget>
+
+class ShadowWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ShadowWidget(QWidget *parent = nullptr);
+
+signals:
+
+
+    // QWidget interface
+protected:
+    void paintEvent(QPaintEvent *event);
+};
+
+#endif // SHADOWWIDGET_H
