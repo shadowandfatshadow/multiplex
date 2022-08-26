0ec0931593b37edd3fc200883e8e255511878bb2
chan <1>
2022-08-26 10:31:02 +0800

    自定义ToolButton
    主要是设置图标和文字间的间隔

---
+++ 自绘带图标的按钮/custombutton.cpp
@@ -0,0 +1,102 @@
+﻿#include "custombutton.h"
+#include <QPainter>
+#include <QStyleOptionToolButton>
+#include <QFontMetrics>
+
+CustomButton::CustomButton(QWidget *parent)
+    :QToolButton(parent)
+{
+
+}
+
+void CustomButton::paintEvent(QPaintEvent *event)
+{
+    QToolButton::paintEvent(event);             //让qss先生效
+
+    //绘制准备工作,启用反锯齿
+    QPainter painter(this);
+    painter.save();
+
+    painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing);
+
+    QRect rect = this->rect();
+    int rectW = rect.width();
+    int rectH = rect.height();
+
+    int padding_left = (rectW - (m_iconSize.width() + m_space + m_textSize.width())) /2;
+
+    QRect iconRect = QRect(rect.left() + padding_left,rect.top() + (rectH - m_iconSize.height())/2,m_iconSize.width(),m_iconSize.height());
+    QRect textRect = QRect(iconRect.right() + m_space,rect.top() + (rectH - m_textSize.height())/2,m_textSize.width(),m_textSize.height());
+
+    QPixmap p = m_icon.pixmap(m_iconSize,
+                              QIcon::Normal,
+                              isChecked() ?  QIcon::On : QIcon::Off);
+
+    painter.drawPixmap(iconRect, p);
+
+    painter.setPen(Qt::green);
+    painter.drawText(textRect, m_text);
+
+    painter.restore();
+}
+
+const QFont &CustomButton::font() const
+{
+    return m_font;
+}
+
+void CustomButton::setFont(const QFont &newFont)
+{
+    m_font = newFont;
+    QFontMetrics f(m_font);
+    m_textSize = QSize(f.width(m_text),f.height());
+}
+
+int CustomButton::space() const
+{
+    return m_space;
+}
+
+void CustomButton::setSpace(int newSpace)
+{
+    m_space = newSpace;
+}
+
+const QSize &CustomButton::iconSize() const
+{
+    return m_iconSize;
+}
+
+void CustomButton::setIconSize(const QSize &newIconSize)
+{
+    m_iconSize = newIconSize;
+}
+
+void CustomButton::setIconSize(int w, int h)
+{
+    m_iconSize = QSize(w,h);
+}
+
+const QIcon &CustomButton::icon() const
+{
+    return m_icon;
+}
+
+void CustomButton::setIcon(const QIcon &newIcon)
+{
+    m_icon = newIcon;
+}
+
+const QString &CustomButton::text() const
+{
+    return m_text;
+}
+
+void CustomButton::setText(const QString &newText)
+{
+    m_text = newText;
+
+    QFontMetrics f(m_font);
+    m_textSize = QSize(f.width(m_text),f.height());
+
+}
---
+++ 自绘带图标的按钮/custombutton.h
@@ -0,0 +1,39 @@
+﻿#ifndef CUSTOMBUTTON_H
+#define CUSTOMBUTTON_H
+
+#include <QToolButton>
+
+class CustomButton : public QToolButton
+{
+    Q_OBJECT
+public:
+    CustomButton(QWidget *parent = nullptr);
+
+    // QWidget interface
+    const QIcon &icon() const;
+    void setIcon(const QIcon &newIcon);
+    const QString &text() const;
+    void setText(const QString &newText);
+
+    const QSize &iconSize() const;
+    void setIconSize(const QSize &newIconSize);
+    void setIconSize(int w,int h);
+
+    int space() const;
+    void setSpace(int newSpace);
+
+    const QFont &font() const;
+    void setFont(const QFont &newFont);
+
+protected:
+    void paintEvent(QPaintEvent *event);
+
+    QIcon m_icon;
+    QSize m_iconSize = QSize(16,16);
+    QString m_text;
+    QSize m_textSize;
+    QFont m_font;
+    int m_space = 10;                           //图片与文字的间隔
+};
+
+#endif // CUSTOMBUTTON_H
