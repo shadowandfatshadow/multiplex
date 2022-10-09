1cb309d4b70a95317fc00ebc554b976a4b3cc059
chan <1>
2022-10-09 10:42:34 +0800

    圆形进度条

---
+++ 圆形进度条/roundprocessbar.cpp
@@ -0,0 +1,162 @@
+﻿#include "roundprocessbar.h"
+#include <QDebug>
+
+RoundProcessBar::RoundProcessBar(QWidget *parent)
+    :QWidget(parent)
+    ,m_value(0.0)
+    ,m_startAngle(90)
+    ,m_max(100)
+{
+    //设置默认外圈宽度
+    setOuterWide(18);
+    //设置默认内圈宽度
+    setInnerWide(16);
+    //设置外圈颜色
+    setOuterColor(QColor(233,248,248));
+    //设置内圈颜色
+    setInnerColor(QColor(133, 243, 244));
+    //设置文字颜色
+    setTextColor(QColor(49,177,190));
+}
+
+RoundProcessBar::~RoundProcessBar()
+{
+
+}
+
+double RoundProcessBar::outerWide() const
+{
+    return m_outerWide;
+}
+
+void RoundProcessBar::setOuterWide(double newOuterWide)
+{
+    m_outerWide = newOuterWide;
+}
+
+double RoundProcessBar::innerWide() const
+{
+    return m_innerWide;
+}
+
+void RoundProcessBar::setInnerWide(double newInnerWide)
+{
+    m_innerWide = newInnerWide;
+}
+
+const QColor &RoundProcessBar::outerColor() const
+{
+    return m_outerColor;
+}
+
+void RoundProcessBar::setOuterColor(const QColor &newOuterColor)
+{
+    m_outerColor = newOuterColor;
+}
+
+const QColor &RoundProcessBar::innerColor() const
+{
+    return m_innerColor;
+}
+
+void RoundProcessBar::setInnerColor(const QColor &newInnerColor)
+{
+    m_innerColor = newInnerColor;
+}
+
+const QColor &RoundProcessBar::textColor() const
+{
+    return m_textColor;
+}
+
+void RoundProcessBar::setTextColor(const QColor &newTextColor)
+{
+    m_textColor = newTextColor;
+}
+
+double RoundProcessBar::value() const
+{
+    return m_value;
+}
+
+void RoundProcessBar::setValue(double newValue)
+{
+    m_value = newValue;
+    repaint();
+}
+
+void RoundProcessBar::paintOuterBar(QPainter &p)
+{
+    QPen pen;
+    pen.setWidth(m_outerWide);
+    pen.setColor(m_outerColor);
+    p.setPen(pen);
+    QRectF r(m_squareStart,m_squareStart,m_squareWide,m_squareWide);
+    p.drawEllipse(r);
+}
+
+void RoundProcessBar::paintInnerBar(QPainter &p)
+{
+    QPen pen;
+    pen.setWidth(m_innerWide);
+    pen.setColor(m_innerColor);
+    pen.setStyle(Qt::SolidLine);
+    pen.setCapStyle(Qt::RoundCap);
+    pen.setJoinStyle(Qt::RoundJoin);
+    p.setPen(pen);
+    QRectF r(m_squareStart,m_squareStart,m_squareWide,m_squareWide);
+
+    //画圆弧的时候是以16分之1度为单位，所以这里要乘以16
+    int startAngle = m_startAngle * 16;
+    int spanAngle = m_value / m_max * 360 * 16 * -1;                //逆时针旋转乘以-1
+    p.drawArc(r,startAngle,spanAngle);
+
+}
+
+void RoundProcessBar::paintText(QPainter &p)
+{
+    p.setPen(m_textColor);
+    p.setFont(QFont("Microsoft YaHei",22));
+    p.drawText(m_squareStart,m_squareStart,m_squareWide,m_squareWide,Qt::AlignCenter,
+                             QString::number(m_value/m_max * 100,'f',1)+"%");
+}
+
+void RoundProcessBar::caculateSquare()
+{
+    int minWide = qMin(width(),height());
+    double barWide = qMax(m_outerWide,m_innerWide);
+    //控件宽度 = 2 * 偏移距离 + 用来画圆的矩形长度
+    m_squareWide = minWide - barWide - 2;
+    m_squareStart = barWide / 2 + 1;
+}
+
+double RoundProcessBar::max() const
+{
+    return m_max;
+}
+
+void RoundProcessBar::setMax(double newMax)
+{
+    m_max = newMax;
+}
+
+void RoundProcessBar::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+
+    paintOuterBar(painter);
+    paintInnerBar(painter);
+    paintText(painter);
+}
+
+void RoundProcessBar::resizeEvent(QResizeEvent *event)
+{
+    //根据内外圈跨度设置控件得最小大小
+    if(m_outerWide > m_innerWide)
+        setMinimumSize(m_outerWide * 4,m_outerWide * 4);
+    else
+        setMinimumSize(m_innerWide * 4,m_innerWide * 4);
+
+    caculateSquare();
+}
---
+++ 圆形进度条/roundprocessbar.h
@@ -0,0 +1,60 @@
+﻿#pragma once
+
+#include <QWidget>
+#include <QPainter>
+
+class RoundProcessBar : public QWidget
+{
+    Q_OBJECT
+
+public:
+    RoundProcessBar(QWidget* parent = nullptr);
+    ~RoundProcessBar();
+
+    double outerWide() const;
+    void setOuterWide(double newOuterWide);
+
+    double innerWide() const;
+    void setInnerWide(double newInnerWide);
+
+    const QColor &outerColor() const;
+    void setOuterColor(const QColor &newOuterColor);
+
+    const QColor &innerColor() const;
+    void setInnerColor(const QColor &newInnerColor);
+
+    const QColor &textColor() const;
+    void setTextColor(const QColor &newTextColor);
+
+    double value() const;
+    void setValue(double newValue);
+
+    double max() const;
+    void setMax(double newMax);
+
+private:
+    void paintOuterBar(QPainter& p);    //绘制外圈
+    void paintInnerBar(QPainter& p);    //绘制内圈
+    void paintText(QPainter& p);        //绘制文字
+    void caculateSquare();              //计算矩形信息，需要是矩形，在画弧的时候才能是圆
+
+private:
+    int m_startAngle;               //初始角度，大部分是从顶部开始，也就是得先转个90度
+    double m_value;
+    double m_max;
+
+    double m_squareStart;           //矩形左上角
+    double m_squareWide;            //举行宽度
+
+    double m_outerWide;             //外圈宽度
+    double m_innerWide;             //内圈宽度
+    QColor m_outerColor;            //外圈颜色
+    QColor m_innerColor;            //内圈颜色
+    QColor m_textColor;             //文字颜色
+
+
+    // QWidget interface
+protected:
+    void paintEvent(QPaintEvent *event);
+    void resizeEvent(QResizeEvent *event);
+};
